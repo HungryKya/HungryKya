637433521d3d912bb9a51be9bab6482a80bc160e
Meng <dream.mengzhou@gmail.com>
2017-10-09 16:47:53 -0700

    added Read.me

--- app/src/main/java/com/codepath/hungrykya/MainActivity.java
+++ app/src/main/java/com/codepath/hungrykya/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fabric.with(this, new Crashlytics());
+
         setContentView(R.layout.activity_main);
     }
 }
